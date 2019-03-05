6ee98090e12b742faca71cacafb05820a5b1c3cc
alejandro <alejoicecream@gmail.com>
2019-03-04 21:45:30 -0500

    creacion de proyecto y clase main

---
+++ polimorfismo/src/main/java/com/linea3/polimorfismo/Inicio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.linea3.polimorfismo;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Inicio {
+    
+     public static void main(String[] args) throws IOException {
+        
+         Menu menuInicial=new Menu();
+         
+         menuInicial.Imprimir();
+    }
+     
+}
